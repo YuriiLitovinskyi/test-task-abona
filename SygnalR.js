fccf8513ff39d9adbc0d1ca7af79cb2bf33bcf24
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2020-03-11 10:47:21 +0200

    some changes

--- SygnalR.js
+++ SygnalR.js
@@ -1,15 +1,32 @@
 (function(){
+    const hub = $.connection.yardHub;
+
     $connection.hub.start()
     .done(function(){
+        $.connection.hub.logging = true;
         console.log("Connected");
-        $.connection.yardHub.BCMAck(operationArguments, token, seq);
+        $.hub.server.BCMAck(operationArguments, token, seq)
+            .done(function(data){
+                writeToPage(data);
+            })
+            .fail(function(error){
+                writeToPage(error);
+            });
     })
     .fail(function(){
         console.log("Error...");
     });
 
-    $.connection.yardHub.client.BCMAck = function(operationArguments, token, seq){
+    $.hub.client.BCMAck = function(operationArguments, token, seq){     //bCMAck
         alert("connected!");
+        writeToPage(operationArguments);
+        //$.hub.client.registerContainerCargoUnit();
+        // $.hub.client.RegisterInLandCargoUnit();
+        //$.hub.client.LeftYard();
+    }
+
+    var writeToPage = function(msg){
+        $("#msg").append(msg + '<br />');
     }
 
 })();
