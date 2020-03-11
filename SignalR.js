9f8969648b5fcf9e65cb13c71aead6e08dbeebd1
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2020-03-11 16:59:45 +0200

    connect to hub

--- SignalR.js
+++ SignalR.js
@@ -1,32 +1,61 @@
 (function(){
-    const hub = $.connection.yardHub;
+    $.support.cors = true;
+    // var signalR = $.signalR;
+    // //$.connection.hub.url = "https://yardcorestandarddevelopment.azurewebsites.net";
+    // signalR.hub = $.hubConnection("https://yardcorestandarddevelopment.azurewebsites.net/signalr");
 
-    $connection.hub.start()
-    .done(function(){
-        $.connection.hub.logging = true;
-        console.log("Connected");
-        $.hub.server.BCMAck(operationArguments, token, seq)
-            .done(function(data){
-                writeToPage(data);
+
+
+    var yardHub = $.hubConnection("https://yardcorestandarddevelopment.azurewebsites.net/");  //signalr
+    //var hub = connection.createHubProxy("BCMAck"); 
+    //var subscription = hub.Subscribe("BCMAck");
+    //var yardHub = $.connection;
+    //var simpleHubProxy = $.connection.yardHub;
+    //registerClientMethods(yardHub); 
+    
+    //hub.on("AddMessage", Method);
+    yardHub.start()
+                .done(function () {
+                //$.connection.hub.logging = true;
+                console.log('Connected... Hub id: ' + yardHub.id);
+                //hub.say("success?");
+                //yardHub.server.BCMAck(operationArguments, token, seq)
+                //BCMAck (bCMAck)
+                
             })
-            .fail(function(error){
-                writeToPage(error);
+            .fail(function (a) {
+                console.log('Not connected '+a);
             });
-    })
-    .fail(function(){
-        console.log("Error...");
-    });
+    
 
-    $.hub.client.BCMAck = function(operationArguments, token, seq){     //bCMAck
-        alert("connected!");
-        writeToPage(operationArguments);
-        //$.hub.client.registerContainerCargoUnit();
-        // $.hub.client.RegisterInLandCargoUnit();
-        //$.hub.client.LeftYard();
-    }
 
-    var writeToPage = function(msg){
-        $("#msg").append(msg + '<br />');
-    }
+
+    // $.signalR.hub.start()
+    // .done(function(){
+    //     $.connection.hub.logging = true;
+    //     console.log("Connected");
+    //     $.signalR.hub.server.BCMAck(operationArguments, token, seq)
+    //         .done(function(data){
+    //             writeToPage(data);
+    //         })
+    //         .fail(function(error){
+    //             writeToPage(error);
+    //         });
+    // })
+    // .fail(function(){
+    //     console.log("Error...");
+    // });
+
+    // $.signalR.hub.client.BCMAck = function(operationArguments, token, seq){     //bCMAck
+    //     alert("connected!");
+    //     writeToPage(operationArguments);
+    //     //$.hub.client.registerContainerCargoUnit();
+    //     // $.hub.client.RegisterInLandCargoUnit();
+    //     //$.hub.client.LeftYard();
+    // }
+
+    // var writeToPage = function(msg){
+    //     $("#msg").append(msg + '<br />');
+    // }
 
 })();
