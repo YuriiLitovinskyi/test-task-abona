71589bfd05a361c29fce8f2d3504c8717e9c0a32
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2020-03-12 09:23:15 +0200

    ...

--- SignalR.js
+++ SignalR.js
@@ -2,12 +2,21 @@
 var hub = $.hubConnection("https://yardcorestandarddevelopment.azurewebsites.net/"); 
 
 hub.logging = true;
+
+var HubProxy = hub.createHubProxy('yardHub');
+HubProxy.on('message', function(operationArguments, token, seq) {
+    console.log(operationArguments, token, seq);
+});
+
 hub.start()
     .done(function () {        
         console.log(`Connected... Hub id: ${hub.id}`);
-        console.log("Transport = " + hub.transport.name);
-    
-        console.log(hub);        
+        console.log(`Transport: ${hub.transport.name}`);    
+        console.log(hub);   
+        
+        $('#click').click(function () {
+            HubProxy.invoke('BCMAck', {}, hub.token, 34);            
+        });
     })
     .fail(function (err) {
         console.log(`Not connected: ${err}`);
