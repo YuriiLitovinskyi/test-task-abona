cde2ede793426491d52e3fddb19c78f9bfc7c22e
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2020-03-16 21:54:00 +0200

    ..

--- SignalR.js
+++ SignalR.js
@@ -9,6 +9,37 @@ HubProxy.on('BCMAck', function(operationArguments, token, seq) {
     console.log(operationArguments);
     console.log(token);
     console.log(seq);
+
+    if (operationArguments.OperationName === 'RegisterContainerCargoUnit') {
+
+        let store = [];
+
+        HubProxy.client.updateStockPrice = function (data) {
+            store.push([{ type: "update", key: data.Symbol, data: data }]);
+        };
+
+
+        let dataGrid = $("#gridContainer").dxDataGrid({
+            // ...
+        }).dxDataGrid("instance");
+        let dataSource = dataGrid.getDataSource();
+        dataSource.store().insert(data).then(function() {
+            dataSource.reload();
+        });
+    }
+
+    // var store = new DevExpress.data.CustomStore({
+    //     load: function() {
+    //         return HubProxy.server.getAllStocks();
+    //     },
+    //     key: "OID"
+    // });
+
+    HubProxy.client.updateStockPrice = function (data) {
+        store.push([{ type: "update", key: data.Symbol, data: data }]);
+    };
+
+    
 });
 
 hub.start()
--- jquery.js
+++ jquery.js
@@ -1,13 +1,56 @@
 $(document).ready(function() {
 
+    var hub = $.hubConnection("https://yardcorestandarddevelopment.azurewebsites.net/signalR"); 
+
+    hub.logging = true;
+
+    var HubProxy = hub.createHubProxy('yardHub');
+
     // GET all transport units
     $.get('https://yardwebapiexp.azurewebsites.net/api/CargoUnits/GetCargoUnitsInYard', function(data) {
     //debugger    
     data.forEach(item => item.Arrival = new Date(item.Arrival));
     console.log(data);
-       
+
+    // var store = new DevExpress.data.CustomStore({
+    //     load: function() {
+    //         return data;
+    //     },
+    //     key: "OID"
+    // });
+
+    HubProxy.on('BCMAck', function(operationArguments, token, seq) {
+        console.log(operationArguments, token, seq);
+        console.log(operationArguments);
+        console.log(token);
+        console.log(seq);
+    
+        if (operationArguments.OperationName === 'RegisterContainerCargoUnit') {
+
+            let dataGrid = $("#gridContainer").dxDataGrid({
+                // ...
+            }).dxDataGrid("instance");
+            let dataSource = dataGrid.getDataSource();
+            dataSource.store().insert(operationArguments.Arguments).then(function() {
+                dataSource.reload();
+            });
+        }
+         
+            // HubProxy.client.updateStockPrice = function (data) {
+            //     store.push([{ type: "update", key: data.OID, data: data }]);
+            // };
+    
+    });
+
+
+    hub.start()
+    .done(function () {        
+        console.log(`Connected... Hub id: ${hub.id}`);
+        console.log(`Transport: ${hub.transport.name}`);    
+        console.log(hub); 
+        
         $('#gridContainer').dxDataGrid({
-            dataSource: data,
+            dataSource: data,  //store
             showRowLines: true,           
             rowAlternationEnabled: true,            
             showBorders: true,
@@ -22,7 +65,8 @@ $(document).ready(function() {
                 'TransportCompanyName',
                 'RegistrationPlate',                           
                 'CargoUnitStatus',
-                'IsLeftYard'                            
+                'IsLeftYard',
+                'Standing'                            
             ],
             paging: {
                 pageSize: 4
@@ -56,7 +100,15 @@ $(document).ready(function() {
                 contextMenuEnabled: true,
                 expandMode: "rowClick"
             }            
-        });        
+        }); 
+      
+    })
+    .fail(function (err) {
+        console.log(`Not connected: ${err}`);
+    });
+
+       
+              
     });  
   });
 
